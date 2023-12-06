f1efb13947cf216c2ea2f750ac61e6247d59b409
Vlad Tymchenko <vlad.timchenko@gmail.com>
2023-12-06 09:11:40 +0100

    updated index et fetchCV

--- app/src/fetchCV.js
+++ app/src/fetchCV.js
@@ -1,19 +1,15 @@
 var xhr = new XMLHttpRequest();
 
-
-
-
 // Faire les requet pour get xml donnes
 
 xhr.open('GET', "./src/cv.xml", true);
-
 
 
 xhr.onreadystatechange = function () {
     if (xhr.readyState === 4 && xhr.status === 200) {
         var xmlDocument = xhr.responseXML;
         data = xmlDocument.documentElement;
-        getVariables(data)
+        getVariables(data);
     }
 }
 
