6717ffe4def12da7f4371c275443c9116db30ae1
Vlad Tymchenko <vlad.timchenko@gmail.com>
2023-12-08 19:29:43 +0100

    ajoute form devoir

---
+++ app/exercices/ex01/scripts/promotion.js
@@ -0,0 +1,8 @@
+console.log("promotion")
+
+const promotions = {
+    "Réductions": ["Coupons", "Ventes Flash", "Remises en argent", "Offres groupées"],
+    "Programmes de fidélité": ["Points de récompense", "Avantages VIP", "Niveaux de fidélité", "Programmes de parrainage"],
+    "Événements spéciaux": ["Soldes saisonniers", "Journées spéciales", "Lancement de produit", "Anniversaires"]
+};
+
